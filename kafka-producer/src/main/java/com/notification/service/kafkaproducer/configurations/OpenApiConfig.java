5f585eef71967eceba95bc7cbbef47edcbb78510
aek <konan.kouame@soprasteria.com>
2020-08-17 16:28:39 +0200

    swagger integration

---
+++ kafka-producer/src/main/java/com/notification/service/kafkaproducer/configurations/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.notification.service.kafkaproducer.configurations;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Spring kafka producer").description(
+                        "Sample producer data."));
+    }
+}
